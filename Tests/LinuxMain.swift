fe448545864367df2643e19140348d417b57b472
Frizlab <fload@me.com>
2019-06-24 03:09:09 +0200

    Enhance some tests and generate linux main

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,33 +1,8 @@
 import XCTest
 
-@testable import UBJSONSerializationTests
+import UBJSONSerializationTests
 
-var tests: [XCTestCaseEntry] = [
-	testCase([
-	]),
-	testCase([
-		("testHighPrecisionNumberParsingFails", HighPrecisionNumberTests.testHighPrecisionNumberParsingFails),
-		("testHighPrecisionNumberStandardParsing", HighPrecisionNumberTests.testHighPrecisionNumberStandardParsing),
-		("testHighPrecisionNumberStandardParsing2", HighPrecisionNumberTests.testHighPrecisionNumberStandardParsing2),
-		("testHighPrecisionNumberStandardParsing3", HighPrecisionNumberTests.testHighPrecisionNumberStandardParsing3),
-		("testHighPrecisionNumberStandardParsing4", HighPrecisionNumberTests.testHighPrecisionNumberStandardParsing4),
-		("testHighPrecisionNumberStandardParsing5", HighPrecisionNumberTests.testHighPrecisionNumberStandardParsing5),
-		("testHighPrecisionNumberStandardParsing6", HighPrecisionNumberTests.testHighPrecisionNumberStandardParsing6),
-	]),
-	testCase([
-	]),
-	testCase([
-		("testGarbageAtEndError", UBJSONSerializationTests.testGarbageAtEndError),
-		("testDecodeNil", UBJSONSerializationTests.testDecodeNil),
-		("testDecodeNop", UBJSONSerializationTests.testDecodeNop),
-		("testNopSkipping", UBJSONSerializationTests.testNopSkipping),
-		("testEncodeNil", UBJSONSerializationTests.testEncodeNil),
-		("testEncodeNop", UBJSONSerializationTests.testEncodeNop),
-		("testEncodeInt8", UBJSONSerializationTests.testEncodeInt8),
-		("testEncodeOptimizedArrayOfInts", UBJSONSerializationTests.testEncodeOptimizedArrayOfInts),
-		("testEncodeSimpleObject", UBJSONSerializationTests.testEncodeSimpleObject),
-		("testDecodeSimpleObject", UBJSONSerializationTests.testDecodeSimpleObject),
-		("testDecodeBigOptimizedArray", UBJSONSerializationTests.testDecodeBigOptimizedArray),
-	]),
-]
+var tests = [XCTestCaseEntry]()
+tests += UBJSONSerializationTests.__allTests()
+
 XCTMain(tests)
--- Tests/UBJSONSerializationTests/HighPrecisionNumberTests.swift
+++ Tests/UBJSONSerializationTests/HighPrecisionNumberTests.swift
@@ -14,86 +14,62 @@ class HighPrecisionNumberTests: XCTestCase {
 		XCTAssertThrowsError(try HighPrecisionNumber(unparsedValue: "1."))
 	}
 	
-	func testHighPrecisionNumberStandardParsing() {
-		do {
-			let n = try HighPrecisionNumber(unparsedValue: "10.00e+001")
-			XCTAssertEqual(n.normalizedStringValue, "10.00e1")
-			XCTAssertFalse(n.intPart.hasNegativeSign)
-			XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
-			XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
-			XCTAssertFalse(n.exponentPart?.hasNegativeSign ?? true)
-			XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d0, .d0, .d1])
-		} catch {
-			XCTFail(error.localizedDescription)
-		}
+	func testHighPrecisionNumberStandardParsing() throws {
+		let n = try HighPrecisionNumber(unparsedValue: "10.00e+001")
+		XCTAssertEqual(n.normalizedStringValue, "10.00e1")
+		XCTAssertFalse(n.intPart.hasNegativeSign)
+		XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
+		XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
+		XCTAssertFalse(n.exponentPart?.hasNegativeSign ?? true)
+		XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d0, .d0, .d1])
 	}
 	
-	func testHighPrecisionNumberStandardParsing2() {
-		do {
-			let n = try HighPrecisionNumber(unparsedValue: "-10.00e1")
-			XCTAssertEqual(n.normalizedStringValue, "-10.00e1")
-			XCTAssertTrue(n.intPart.hasNegativeSign)
-			XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
-			XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
-			XCTAssertFalse(n.exponentPart?.hasNegativeSign ?? true)
-			XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d1])
-		} catch {
-			XCTFail(error.localizedDescription)
-		}
+	func testHighPrecisionNumberStandardParsing2() throws {
+		let n = try HighPrecisionNumber(unparsedValue: "-10.00e1")
+		XCTAssertEqual(n.normalizedStringValue, "-10.00e1")
+		XCTAssertTrue(n.intPart.hasNegativeSign)
+		XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
+		XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
+		XCTAssertFalse(n.exponentPart?.hasNegativeSign ?? true)
+		XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d1])
 	}
 	
-	func testHighPrecisionNumberStandardParsing3() {
-		do {
-			let n = try HighPrecisionNumber(unparsedValue: "-10.00E-01")
-			XCTAssertEqual(n.normalizedStringValue, "-10.00e-1")
-			XCTAssertTrue(n.intPart.hasNegativeSign)
-			XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
-			XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
-			XCTAssertTrue(n.exponentPart?.hasNegativeSign ?? false)
-			XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d0, .d1])
-		} catch {
-			XCTFail(error.localizedDescription)
-		}
+	func testHighPrecisionNumberStandardParsing3() throws {
+		let n = try HighPrecisionNumber(unparsedValue: "-10.00E-01")
+		XCTAssertEqual(n.normalizedStringValue, "-10.00e-1")
+		XCTAssertTrue(n.intPart.hasNegativeSign)
+		XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
+		XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
+		XCTAssertTrue(n.exponentPart?.hasNegativeSign ?? false)
+		XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d0, .d1])
 	}
 	
-	func testHighPrecisionNumberStandardParsing4() {
-		do {
-			let n = try HighPrecisionNumber(unparsedValue: "-10.00")
-			XCTAssertEqual(n.normalizedStringValue, "-10.00")
-			XCTAssertTrue(n.intPart.hasNegativeSign)
-			XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
-			XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
-			XCTAssertNil(n.exponentPart)
-		} catch {
-			XCTFail(error.localizedDescription)
-		}
+	func testHighPrecisionNumberStandardParsing4() throws {
+		let n = try HighPrecisionNumber(unparsedValue: "-10.00")
+		XCTAssertEqual(n.normalizedStringValue, "-10.00")
+		XCTAssertTrue(n.intPart.hasNegativeSign)
+		XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
+		XCTAssertEqual(n.decimalPart ?? [], [.d0, .d0])
+		XCTAssertNil(n.exponentPart)
 	}
 	
-	func testHighPrecisionNumberStandardParsing5() {
-		do {
-			let n = try HighPrecisionNumber(unparsedValue: "-10")
-			XCTAssertEqual(n.normalizedStringValue, "-10")
-			XCTAssertTrue(n.intPart.hasNegativeSign)
-			XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
-			XCTAssertNil(n.decimalPart)
-			XCTAssertNil(n.exponentPart)
-		} catch {
-			XCTFail(error.localizedDescription)
-		}
+	func testHighPrecisionNumberStandardParsing5() throws {
+		let n = try HighPrecisionNumber(unparsedValue: "-10")
+		XCTAssertEqual(n.normalizedStringValue, "-10")
+		XCTAssertTrue(n.intPart.hasNegativeSign)
+		XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
+		XCTAssertNil(n.decimalPart)
+		XCTAssertNil(n.exponentPart)
 	}
 	
-	func testHighPrecisionNumberStandardParsing6() {
-		do {
-			let n = try HighPrecisionNumber(unparsedValue: "-10e1500")
-			XCTAssertEqual(n.normalizedStringValue, "-10e1500")
-			XCTAssertTrue(n.intPart.hasNegativeSign)
-			XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
-			XCTAssertNil(n.decimalPart)
-			XCTAssertFalse(n.exponentPart?.hasNegativeSign ?? true)
-			XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d1, .d5, .d0, .d0])
-		} catch {
-			XCTFail(error.localizedDescription)
-		}
+	func testHighPrecisionNumberStandardParsing6() throws {
+		let n = try HighPrecisionNumber(unparsedValue: "-10e1500")
+		XCTAssertEqual(n.normalizedStringValue, "-10e1500")
+		XCTAssertTrue(n.intPart.hasNegativeSign)
+		XCTAssertEqual(n.intPart.unsignedIntPart, [.d1, .d0])
+		XCTAssertNil(n.decimalPart)
+		XCTAssertFalse(n.exponentPart?.hasNegativeSign ?? true)
+		XCTAssertEqual(n.exponentPart?.unsignedIntPart ?? [], [.d1, .d5, .d0, .d0])
 	}
 	
 }
